453bf6b5e26a91191326a9a66f5c090debbdb754
zblasingame <blasinzw@clarkson.edu>
2016-11-15 03:18:25 -0500

    Updated webpage

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
 				$('.scroll-up').fadeOut();
 			}
 		});
-		
+
 		//navbar
 		$('.header').sticky({
 			topSpacing: 0
@@ -32,6 +32,29 @@
 			target: '.navbar-custom',
 			offset: 70
 		});
+
+		//google maps
+		function init_map() {
+			var location = new google.maps.LatLng(44.231505, -76.109560);
+
+			var map_canvas = document.getElementById('map');
+			var map_options = {
+				center: location,
+				zoom: 14,
+				panControl: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+			var map = new google.maps.Map(map_canvas, map_options);
+
+			var marker = new google.maps.Marker({
+				position: location,
+				map: map
+			})
+
+		}
+
+		google.maps.event.addDomListener(window, 'load', init_map);
 
 	});
 })(jQuery);
